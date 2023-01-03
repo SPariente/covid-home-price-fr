e5fc549e8a7f1acfea43c91a6f1a6438a10ca468
Samuel Pariente <samuel.pariente@hotmail.fr>
2023-01-03 22:47:05 +0100

    Added SQL queries on home prices

---
+++ price_query_marseille.sql
@@ -0,0 +1,26 @@
+SELECT
+id_mutation,
+MAX(valeur_fonciere) AS price,
+nature_mutation,
+SUM(surface_reelle_bati) AS total_surface,
+SUM(
+	COALESCE(lot1_surface_carrez, 0)+
+	COALESCE(lot2_surface_carrez, 0)+
+	COALESCE(lot3_surface_carrez, 0)+
+	COALESCE(lot4_surface_carrez, 0)+
+	COALESCE(lot5_surface_carrez, 0)
+) AS carrez_surface,
+DATE_PART('year', date_mutation) AS sale_year,
+DATE_PART('week', date_mutation) AS sale_week,
+MAX(id_parcelle) AS id_parcelle,
+MAX(nom_commune) AS nom_commune
+FROM fr_home_sales
+WHERE 
+	LOWER(nom_commune) LIKE 'marseille %'
+AND id_mutation IN (
+	SELECT id_mutation FROM fr_home_sales
+	WHERE type_local IN ('Appartement', 'Maison')
+	AND nature_mutation='Vente'
+)
+GROUP BY id_mutation, nature_mutation, sale_year, sale_week
+;
