bdf715e95e3015767df4ae6fbf85f532ff8d8968
Samuel Pariente <samuel.pariente@hotmail.fr>
2023-01-04 16:36:54 +0100

    Revised order of subqueries, significant time improvement + added comments

--- price_query_combined.sql
+++ price_query_combined.sql
@@ -1,30 +1,37 @@
 SELECT
 *,
-CASE
+CASE -- Compute price per sq. meter (NULL if impossible to compute)
 	WHEN paid_surface>0 THEN price/paid_surface
 	ELSE NULL
 END AS price_sqm
+
 FROM(
-	WITH temp_table as(
+	WITH subquery as(
 		SELECT
 		id_mutation,
 		MAX(valeur_fonciere) AS price,
 		nature_mutation,
 		SUM(surface_reelle_bati) AS total_surface,
 		SUM(
-			COALESCE(lot1_surface_carrez, 0)+
+			COALESCE(lot1_surface_carrez, 0)+ -- Avoid NULL values in SUM
 			COALESCE(lot2_surface_carrez, 0)+
 			COALESCE(lot3_surface_carrez, 0)+
 			COALESCE(lot4_surface_carrez, 0)+
 			COALESCE(lot5_surface_carrez, 0)
 		) AS carrez_surface,
-		DATE_PART('year', date_mutation) AS sale_year,
-		DATE_PART('week', date_mutation) AS sale_week,
+		DATE_PART('year', date_mutation) AS sale_year, -- Extract year
+		DATE_PART('week', date_mutation) AS sale_week, -- Extract week in year
 		MAX(id_parcelle) AS id_parcelle,
 		MAX(nom_commune) AS nom_commune
-		FROM fr_home_sales
+		FROM ( -- Subquery to extract specific cities, creates time issues if part of main_query
+			SELECT * FROM fr_home_sales
+			WHERE -- Only focus on a few (large) cities for POC
+			LOWER(nom_commune) LIKE 'paris %'
+			OR LOWER(nom_commune) LIKE 'lyon %'
+			OR LOWER(nom_commune) LIKE 'marseille %'
+		) AS loc_query
 		WHERE id_mutation IN (
-			SELECT id_mutation FROM fr_home_sales
+			SELECT id_mutation FROM fr_home_sales -- Only include simple sales of residential house and apartments
 			WHERE type_local IN ('Appartement', 'Maison')
 			AND nature_mutation='Vente'
 		)
@@ -33,14 +40,10 @@ FROM(
 	
 	SELECT 
 	*, 
-	CASE
+	CASE -- Consider the total surface as proxy for carrez_surface if the latter is missing (should be a maximizer)
 		WHEN carrez_surface>0 THEN carrez_surface
 		WHEN NOT carrez_surface>0 AND total_surface>0 THEN total_surface
 		ELSE NULL
 	END AS paid_surface
-	FROM temp_table
-	WHERE 
-	LOWER(nom_commune) LIKE 'paris %'
-	OR LOWER(nom_commune) LIKE 'lyon %'
-	OR LOWER(nom_commune) LIKE 'marseille %'
-) AS subquery;
+	FROM subquery	
+) AS main_query;
